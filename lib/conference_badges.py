bfe30772d43eaa573dd803e7aea82e348697cd4b
Mark Shkreli <m.shkreli96@icloud.com>
2023-04-01 16:23:45 -0400

    Lab Done

--- lib/conference_badges.py
+++ lib/conference_badges.py
@@ -1,11 +1,19 @@
 def badge_maker(name):
-    return None
+    return (f"Hello, my name is {name}.")
 
 def batch_badge_creator(names):
-    return None
+    badge_list = ["Hello, my name is " + i + "." for i in names]
+    return badge_list
 
 def assign_rooms(names):
-    return None
+    rooms = range(1,9)
+    room_list = [f"Hello, {speaker}! You'll be assigned to room {room}!" for speaker, room in zip(names, rooms)] 
+    return room_list
 
 def printer(names):
-    return None
+    badges = batch_badge_creator(names)
+    for badge in badges:
+        print(badge)
+    rooms = assign_rooms(names)
+    for room in rooms:
+        print(room)
